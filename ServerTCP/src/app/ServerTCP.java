d8536ab8ef24aad70b354586b209309b7c7ad430
Osman Khoder <osmankhoder7@gmail.com>
2019-02-23 19:07:56 +0200

    Copied PascalFares ServerTCP Code.

--- ServerTCP/src/app/ServerTCP.java
+++ ServerTCP/src/app/ServerTCP.java
@@ -1,6 +1,7 @@
 package app;
 
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerTCP {
@@ -15,7 +16,25 @@ public class ServerTCP {
         return new PrintWriter(new OutputStreamWriter(p.getOutputStream()));
     }
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException
+    {
+        ServerSocket ecoute;
+        ecoute = new ServerSocket(2000);
+        System.out.printf("L'adresse de la socket d'ecoute est %s\n",
+                ecoute.getLocalSocketAddress());
+        while(true)
+        {
+            Socket serviceSocket = ecoute.accept();
+            System.out.printf("L'adresse de la socket cliente (remote) est %s\n",
+                    serviceSocket.getRemoteSocketAddress());
+            BufferedReader ir = getInput(serviceSocket);
+            PrintWriter reply = getOutput(serviceSocket);
+            String line;
+            line = ir.readLine();
+            System.out.printf("Recu %s du Client\n", line);
+            reply.printf("Reponse Serveur %s\n", line);
+            reply.flush();
+            serviceSocket.close();
+        }
     }
 }
