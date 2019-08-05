c6b7e461b46502f8f43e5d193598343f1336eda4
Osman Khoder <osman@merchantwise.com>
2019-08-05 10:44:16 +0300

    Make Broadcasting server continuously respond for fetch requests.

--- ServerTCP/src/app/BroadcastingServer.java
+++ ServerTCP/src/app/BroadcastingServer.java
@@ -1,33 +1,55 @@
 package app;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.io.IOException;
+import java.net.*;
 
 public class BroadcastingServer implements Runnable {
+    private BroadcastListener bl;
+
     @Override
     public void run() {
         try {
-            /*
-             * open receive datagram broadcast socket port 9100
-             */
-            DatagramSocket socket = new DatagramSocket(2001, InetAddress.getByName("0.0.0.0"));
+            bl = new BroadcastListener();
+            bl.listen();
+        } catch (Exception e) {
+            bl.close();
+            e.printStackTrace();
+        }
+    }
+
+    class BroadcastListener {
+        DatagramSocket socket;
+
+        BroadcastListener() throws SocketException, UnknownHostException {
+            socket = new DatagramSocket(2001, InetAddress.getByName("0.0.0.0"));
             socket.setBroadcast(true);
+        }
+
+        void listen() throws IOException {
+            System.out.println("Listening to clients broadcast.");
 
             //Receive a packet
             byte[] recvBuf = new byte[15000];
             DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
+
             socket.receive(packet); // This method blocks until a datagram is received
 
+            greetBroadcaster(packet);
+
+            if (!socket.isClosed()) {
+                listen();
+            }
+        }
+
+        private void greetBroadcaster (DatagramPacket packet) throws IOException {
             byte[] sendData = "SERVER_SCAN".getBytes();
             //Send a response
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, packet.getAddress(), packet.getPort());
             socket.send(sendPacket);
+        }
 
-            // close socket
+        void close () {
             socket.close();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
